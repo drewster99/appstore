4e59791afa2faf19114beea3e69ebb0937a3de8b
Andrew Benson <db@nuclearcyborg.com>
2026-02-13 01:54:24 -0600

    Release v0.0.4

--- appstore/main.swift
+++ appstore/main.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0136628bdebbe927dce02ed57e50baa54177be0d1462d312a7d3de3f598c9c34
+oid sha256:2a302c9678e5d530d6ce59e26e4983a6fb9018c1b876ab43bdbdf2fc66ab7790
 size 2247
